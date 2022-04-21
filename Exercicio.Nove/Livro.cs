34db5f89a24b6dae901df4dd97c30712f1b9af7b
Alessandra Railda <apavezi@gmail.com>
2022-04-20 21:03:24 -0300

    Initial commit

---
+++ Exercicio.Nove/Livro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio.Nove
+{
+    internal struct Livro
+    {
+
+       // private string teste;
+
+        public string Titulo { get; private set; } 
+
+        public string Autor { get; private set; }
+
+        public decimal Valor { get; private set; }
+
+        public Livro(string titulo, string autor, decimal valor)
+        {
+            Titulo = titulo ?? throw new ArgumentNullException(nameof(titulo));
+            Autor = autor ?? throw new ArgumentNullException(nameof(autor));
+            Valor = valor;
+            //this.teste = teste;
+        }
+
+        public override string ToString() => $"Autor: {Autor} \n Livro: {Titulo} \n Valor: {Valor}";
+    }
+}
